2c43ebf94546c7dded9118ef0c733aa89ff3bc72
gaffo <gaffo@twitch.tv>
2024-06-08 12:05:51 -0700

    Checkpoint, almost to execution

--- processor.go
+++ processor.go
@@ -1,7 +1,14 @@
 package jorb
 
+import (
+	"fmt"
+)
+
 // Job represents the current processing state of any job
 type Job[JC any] struct {
+	Id string
+	C JC
+	State string
 }
 
 // Run run manages state of the work a processor is doing
@@ -11,8 +18,8 @@ type Run[OC any, JC any] struct {
 	Overall OC
 }
 
-func NewRun[OC any, JC any](name string, OC oc) {
-	return &Run{
+func NewRun[OC any, JC any](name string, oc OC) *Run[OC,JC] {
+	return &Run[OC, JC]{
 		Name: name,
 		Jobs: map[string]Job[JC]{},
 		Overall: oc,
@@ -20,20 +27,36 @@ func NewRun[OC any, JC any](name string, OC oc) {
 }
 
 // Add a job to the pool, this shouldn't be called once it's running
-func (r *Run[OC any, JC any]) AddJob(JC jc) {
+func (r *Run[OC, JC]) AddJob(jc JC) {
 	// TODO: Use a uuid for the jobs
-	r.Jobs[fmt.Sprintf("%d", len(r.Jobs)) = jc
+	id := fmt.Sprintf("%d", len(r.Jobs))
+	r.Jobs[id] = Job[JC]{
+		Id: id,
+		C: jc,
+	}
+}
+
+const (
+	TRIGGER_STATE_NEW = "new"
+)
+type State[AC any, OC any, JC any] struct {
+	TriggerState string
+	Exec *func(ac AC, oc OC, jc JC) (JC, string, error)
+	Terminal bool
 }
 
 // Processor executes a job
 type Processor[AC any, OC any, JC any] struct {
+	AppContext AC
+	States []State[AC, OC, JC]
 }
 
-func NewProcessor[AC any, OC any, JC any]r() (*Processor[AC, OC, JC]){
+func NewProcessor[AC any, OC any, JC any](ac AC, states []State[AC,OC,JC]) (*Processor[AC, OC, JC]){
 	return &Processor[AC,OC,JC]{
+		AppContext: ac,
+		States: []State[AC,OC,JC]{},
 	}
 }
 
-func (p *Processor[AC any, OC any, JC any]) Exec(Run[OC,JC]) {
-
+func (p *Processor[AC, OC, JC]) Exec(r *Run[OC,JC]) {
 }
--- processor_test.go
+++ processor_test.go
@@ -1,5 +1,9 @@
 package jorb
 
+import (
+	"testing"
+)
+
 // import "github.com/stretchr/testify/assert"
 
 // JobContext represents my Job's context, eg the state of doing work
@@ -17,15 +21,45 @@ type MyAppContext struct {
 
 }
 
+const (
+	STATE_DONE = "done"
+)
+
 func TestProcessorOneJob(t *testing.T) {
-	oc := &MyOverallContext{}
-	ac := &MyAppContext{}
-	r := &NewRun[MyOverallContext, MyJobContext]("job", oc)
+	oc := MyOverallContext{}
+	ac := MyAppContext{}
+	r := NewRun[MyOverallContext, MyJobContext]("job", oc)
 	for i := 0; i < 30; i++ {
-		r.AddJob(&MyJobContext{
+		r.AddJob(MyJobContext{
 			Count: 0,
 		})
 	}
-	p := NewProcessor[MyAppContext, MyOverallContext, MyJobContext](ac)
-	p.Execute(r)
+
+	newFunc := func(ac MyAppContext, oc MyOverallContext, jc MyJobContext) (MyJobContext, string, error) {
+		jc.Count += 1
+		return jc, STATE_DONE, nil
+	}
+
+	states := []State[MyAppContext, MyOverallContext, MyJobContext]{
+		State[MyAppContext, MyOverallContext, MyJobContext]{
+			TriggerState: TRIGGER_STATE_NEW,
+			Exec: &newFunc,
+			Terminal: false,
+		},
+		State[MyAppContext, MyOverallContext, MyJobContext]{
+			TriggerState: TRIGGER_STATE_NEW,
+			Exec: nil,
+			Terminal: false,
+		},
+	}
+
+
+	p := NewProcessor[MyAppContext, MyOverallContext, MyJobContext](ac, states)
+	p.Exec(r)
+
+	for _, j := range r.Jobs {
+		if j.C.Count != 1 {
+			t.Errorf("Expected Job %s Count to be 1 but was %d", j.Id, j.C.Count)
+		}
+	}
 }
